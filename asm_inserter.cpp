ae96d46b2e46766c151e4a82e5a020fed6ea0e8d
Mikhail Kaskov <kaskov.ma@phystech.edu>
2021-04-22 09:40:18 +0300

    Added getting function names and loading to dictionary

--- asm_inserter.cpp
+++ asm_inserter.cpp
@@ -7,6 +7,12 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
+
+#include <iostream>
+#include <cstdio>
+#include <map>
+
+
 using namespace llvm;
 
 namespace {
@@ -15,6 +21,9 @@ struct AsmInserter : public FunctionPass {
   AsmInserter() : FunctionPass(ID) {}
 
   bool runOnFunction(Function &F) override {
+    FILE* log_file = fopen ("logs.log", "w");
+    std::map<std::string, Function*> funcs_map;
+
     bool changed = false;
     for (BasicBlock& BB : F) {
       for (Instruction& I : BB) {
@@ -25,14 +34,29 @@ struct AsmInserter : public FunctionPass {
         if (CI->getCalledFunction()->isIntrinsic()) {
           continue;
         }
-        auto fp_type = PointerType::get(CI->getFunctionType(), 0);
-        auto iasm_type = FunctionType::get(fp_type, {fp_type}, false);
-        auto iasm = InlineAsm::get(iasm_type, "", "=r,0", false);
-        auto iasm_call_t = cast<FunctionType>(iasm->getType()->getPointerElementType());
-        auto call = CallInst::Create(iasm_call_t, iasm, CI->getCalledFunction(), "", CI);
-        CI->setCalledFunction(CI->getFunctionType(), call);
+        Function* func = CI->getCalledFunction();
+        std::string name_func = std::string (func->getName());
+
+        auto ret_search = funcs_map.find (name_func);
+        if (ret_search != funcs_map.end()) {
+          fprintf (log_file, "Function \"%-20s\" - \"%-20s\"\tFOUND\n", std::string (F.getName()).c_str(), name_func.c_str());
+        }
+        else {
+          fprintf (log_file, "Function \"%-20s\" - \"%-20s\"\tNOT FOUND\n", std::string (F.getName()).c_str(), name_func.c_str());
+          auto fp_type = PointerType::get(CI->getFunctionType(), 0);
+          auto iasm_type = FunctionType::get(fp_type, {fp_type}, false);
+          auto iasm = InlineAsm::get(iasm_type, "", "=r,0", false);
+          auto iasm_call_t = cast<FunctionType>(iasm->getType()->getPointerElementType());
+          auto call = CallInst::Create(iasm_call_t, iasm, CI->getCalledFunction(), "", CI);
+          CI->setCalledFunction(CI->getFunctionType(), call);
+
+          funcs_map[name_func] = func;
+
+          // CI->getModule()->getOrInsertFunction();
+        }
       }
     }
+    fclose (log_file);
     return changed;
   }
 }; // end of struct AsmInserter

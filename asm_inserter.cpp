7cc9984f67a300f06b782077d2724d6d4d9356c8
Mikhail Kaskov <kaskov.ma@phystech.edu>
2021-04-23 15:25:18 +0300

    Optimization implemented

--- asm_inserter.cpp
+++ asm_inserter.cpp
@@ -22,105 +22,46 @@ struct AsmInserter : public FunctionPass {
 
   struct DataAboutFunction{
     Function* func;
-    // AllocaInst* alloc;
-    LoadInst* load;
+    Value*    call;
   };
 
   bool runOnFunction(Function &F) override {
-    FILE* log_file = fopen ("logs.log", "w");
     std::map<std::string, DataAboutFunction> funcs_map;
 
     bool changed = false;
     for (BasicBlock& BB : F) {
-      // llvm::Type *type = Type::getInt32Ty(F.getContext());
-      // llvm::AllocaInst (type, 0, "", &(BB.front()));
-
       for (Instruction& I : BB) {
-        if (I.getParent() != &BB)
-          fprintf (log_file, "Parent not equal!\n");
         if (I.getOpcode() != Instruction::Call) {
           continue;
         }
-        // I.dump();
-        // llvm::Type *type = Type::getInt32Ty(F.getContext());
-        // AllocaInst *alloc = new llvm::AllocaInst (type, 0, "", &I);
-
         auto CI = cast<CallInst>(&I);
-        
-        if (CI->getCalledFunction()->isIntrinsic()) {
+        Function* func = CI->getCalledFunction();
+
+        if (func->isIntrinsic()) {
           continue;
         }
-        Function* func = CI->getCalledFunction();
-        std::string name_func = std::string (func->getName());
-
-        auto fp_type = PointerType::get(CI->getFunctionType(), 0);
+        std::string name_func (func->getName());
 
         auto ret_search = funcs_map.find (name_func);
-
         if (ret_search != funcs_map.end()) {
-          fprintf (log_file, "Function \"%-20s\" - \"%-20s\"\tFOUND\n", std::string (F.getName()).c_str(), name_func.c_str());
-          // StoreInst* store = new StoreInst(CI->getCalledFunction(), ret_search->second.alloc, CI);
-          // LoadInst* load = new LoadInst(fp_type, ret_search->second.alloc, "", CI);
-          CI->setCalledFunction(CI->getFunctionType(), ret_search->second.load);
+          CI->setCalledFunction(CI->getFunctionType(), ret_search->second.call);
         }
         else {
           Instruction* insert_instr = &*(F.getEntryBlock().getFirstInsertionPt());
-          AllocaInst* alloc = new AllocaInst(fp_type, 0, "", insert_instr);
-          StoreInst* store = new StoreInst(CI->getCalledFunction(), alloc, insert_instr);
-          LoadInst* load = new LoadInst(fp_type, alloc, "", insert_instr);
-          // CI->setCalledFunction(CI->getFunctionType(), load);
 
-
-          fprintf (log_file, "Function \"%-20s\" - \"%-20s\"\tNOT FOUND\n", std::string (F.getName()).c_str(), name_func.c_str());
+          auto fp_type = PointerType::get(CI->getFunctionType(), 0);
           auto iasm_type = FunctionType::get(fp_type, {fp_type}, false);
           auto iasm = InlineAsm::get(iasm_type, "", "=r,0", false);
           auto iasm_call_t = cast<FunctionType>(iasm->getType()->getPointerElementType());
-
-
-          auto call = CallInst::Create(iasm_call_t, iasm, load, "", CI);
+          auto call = CallInst::Create(iasm_call_t, iasm, func, "", insert_instr);
           CI->setCalledFunction(CI->getFunctionType(), call);
-          // CI->setCalledFunction(CI->getFunctionType(), load);
 
-          funcs_map[name_func] = {func, /*alloc*/load};
-
-          // delete load;
-          // delete store;
-
-          // Allocate mem for point
-          // IntegerType::get(llvm::getGlobalContext(), 32); 
-          // Type *void_type = Type::getVoidTy(F.getContext())->getPointerTo(0);
-
-          // Type *void_type = Type::getPrimitiveType (F.getContext(), Type::TokenTyID);
-          // Type *void_type = llvm::Type::getInt8PtrTy (F.getContext());
-
-          
-
-          // std::vector<value*> vec;
-          // ArrayRef<Value*>* array_ref = new ArrayRef<Value*>(func->getArg(0));
-
-          // auto new_call = CallInst::Create(CI->getFunctionType(), load, func->getFunctionType()->params(), "", CI);
-          // new_call->setCalledFunction(func);
-
-          // CI->getModule()->getOrInsertFunction();
-
-
+          funcs_map[name_func] = {func, call};
         }
       }
     }
-    fclose (log_file);
     return changed;
   }
-
-  // bool runOnFunction(Function &F) override {
-  //   bool changed = false;
-  //   for (BasicBlock& BB : F) {
-  //     for (Instruction& I : BB) {
-  //       llvm::Type *type = Type::getInt32Ty(F.getContext());
-  //       AllocaInst* smth = new llvm::AllocaInst (type, 0, "", &I);
-  //     }
-  //   }
-  //   return changed;
-  // }
 }; // end of struct AsmInserter
 }  // end of anonymous namespace
 
